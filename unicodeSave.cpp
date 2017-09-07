918b0a02d5dabfc73922dd4486b9277fe8d4476c
wstory21 <31491183+wstory21@users.noreply.github.com>
2017-09-07 00:02:29 -0600

    Update unicodeSave.cpp

--- unicodeSave.cpp
+++ unicodeSave.cpp
@@ -3,25 +3,16 @@
 #include <fstream>
 #include <string>
 #include <bitset>
-#include <stack>
+#include <queue>
 
 using namespace std;
 
-struct Node
-{
-	bitset<8> letter;
-	Node *next;
-};
-
-
 int main()
 {
-	Node *inn = new Node;
 	ifstream inf;
-
 	inf.open("input.txt");
 
-	stack<bitset<8>> letters;
+	queue<bitset<8>> letters;
 
 	int size = 0;
 
@@ -31,55 +22,25 @@ int main()
 		letters.push(l);
 		size++;
 	}
+	inf.close();
 
 	ofstream otf;
 	otf.open("outputDec.txt");
+	//converting to bin
 	for (int i = size; i > 0; i--)
 	{
-		otf << letters.top() << " ";
+		otf << letters.front() << " ";
 		letters.pop();
 	}
 
-	/*
-	ifstream inf;
+	//Converting to hex
 	inf.open("input.txt");
-	bitset<8> first (inf.get());
-	bitset<8> second(inf.get());
-	bitset<8> third(inf.get());
-	bitset<8> fourth(inf.get());
-
-
-	ofstream otf;
-	otf.open("outputDec.txt");
-	otf << first << endl << second << endl << third << endl << fourth;
-	*/
-
-
-
-
-
-
-
-
-	/*
-	char uni[100];
-	char buffer[100];
-
-	ifstream inf;
-	inf.open("input.txt");
-
-	ofstream otf;
-	otf.open("output.bin", ios::out | ios::binary);
-
-	int size = 0;
 	while (!(inf.eof()))
 	{
-		inf >> uni[size];
+		bitset<8> l(inf.get());
+		letters.push(l);
 		size++;
 	}
 
-	otf.write(uni, size);
-	otf.close();
-	otf.open("outputDec.txt");
-	*/
+
 }
