04db0bc032650d97fb75e1f7ab516cccb28e7d22
wstory21 <31491183+wstory21@users.noreply.github.com>
2017-10-26 13:42:04 -0600

    Update unicodeSave.cpp

--- unicodeSave.cpp
+++ unicodeSave.cpp
@@ -7,46 +7,74 @@
 
 using namespace std;
 
-queue<bitset<8>> filterOut(queue<bitset<8>> &letters, int &size)
+void binToHex(string s)
 {
-	queue<bitset<8>> Fletters;
-	int count = 1;
-	for (int i = size; i > 0; i--)
-	{
-		if (count == 1 || count == 2 || count == 3)
+	string num;
+		switch (sum)
 		{
-			letters.pop();
-			count++;
-			size--;
+		case 0:
+			num += '0';
+			break;
+		case 1:
+			num += '1';
+			break;
+		case 2:
+			num += '2';
+			break;
+		case 3:
+			num += '3';
+			break;
+		case 4:
+			num += '4';
+			break;
+		case 5:
+			num += '5';
+			break;
+		case 6:
+			num += '6';
+			break;
+		case 7:
+			num += '7';
+			break;
+		case 8:
+			num += '8';
+			break;
+		case 9:
+			num += '9';
+			break;
+		case 10:
+			num += 'A';
+			break;
+		case 11:
+			num += 'B';
+			break;
+		case 12:
+			num += 'C';
+			break;
+		case 13:
+			num += 'D';
+			break;
+		case 14:
+			num += 'E';
+			break;
+		case 15:
+			num += 'F';
+			break;
+		default:
+			cout << "Didn't Work!" << endl;
 		}
-		else if (letters.front().to_ulong() == 255)
-		{
-			letters.pop();
-			count = 1;
-			size--;
-		}
-		else
-		{
-			Fletters.push(letters.front());
-			letters.pop();
-			count++;
-		}
-	}
-	return Fletters;
 }
 
-void printBytes(queue<bitset<8>> fLetters, queue<int> bNum, int size)
+void getHex(queue<bitset<8>> fLetters, queue<int> bNum, int size, queue<char> &hex)
 {
-	queue<char> hex;
 	string num = "";
+	int four = 1;
 	int sum = 0;
 	int count;
-	int four = 1;
 	for (int i = 0; i < size; i++)
 	{
 		count = 1;
 		string yo = fLetters.front().to_string();
-		cout << "Binary: " << yo << endl;
 
 		for (int j = 0; j < 8; j++)
 		{
@@ -124,17 +152,50 @@ void printBytes(queue<bitset<8>> fLetters, queue<int> bNum, int size)
 		}
 		hex.push(num[1]);
 		hex.push(num[0]);
-		cout << "Hex UTF_8: " << num[1] << num[0] << endl << endl;
 		num = "";
 		fLetters.pop();
 	}
+}
 
-	cout << "Unicode Character UTF_8 in hexadecimal: ";
+queue<bitset<8>> filterOut(queue<bitset<8>> &letters, int &size)
+{
+	queue<bitset<8>> Fletters;
+	int count = 1;
+	for (int i = size; i > 0; i--)
+	{
+		if (count == 1 || count == 2 || count == 3)
+		{
+			letters.pop();
+			count++;
+			size--;
+		}
+		else if (letters.front().to_ulong() == 255)
+		{
+			letters.pop();
+			count = 1;
+			size--;
+		}
+		else
+		{
+			Fletters.push(letters.front());
+			letters.pop();
+			count++;
+		}
+	}
+	return Fletters;
+}
+
+void printBytes(queue<bitset<8>> fLetters, queue<int> bNum, int size)
+{
+	queue<char> hex;
+	getHex(fLetters, bNum, size, hex);
+
+	cout << "Unicode Character UTF_8 in hexadecimal" << endl;
 	for (int i = 1; i < size * 2; i = i + 2)
 	{
 		if (bNum.front() == 0)
 		{
-			cout << " : ";
+			cout << ": ";
 			bNum.pop();
 		}
 		cout << hex.front();
@@ -143,7 +204,7 @@ void printBytes(queue<bitset<8>> fLetters, queue<int> bNum, int size)
 		hex.pop();
 		bNum.front()--;
 	}
-	cout << endl;
+	cout << endl << endl;
 }
 
 void getbNum(queue <bitset<8>> letters, queue<int> &num, int size)
@@ -157,18 +218,62 @@ void getbNum(queue <bitset<8>> letters, queue<int> &num, int size)
 		front = letters.front().to_string();
 		if ((front[0] == '1' && front[1] == '1' ) || front[0] == '0')
 		{
-			cout << "worked: " << letters.front()[0] << letters.front()[1] << endl;
 			num.push(count + 0);
 			count = 1;
 		}
 		else
 		{
-			cout << "letters: " << letters.front() << endl;
 			count++;
 		}
 		letters.pop();
 	}
 	num.push(count + 0);
+}
+
+void getCodePoint(queue<bitset<8>> fLetters, queue<int> bNum, int size)
+{
+	queue<char> hex;
+	int count = 1;
+	getHex(fLetters, bNum, size, hex);
+	for (int i = 0; i < size; i++)
+	{
+		if (bNum.front() == 1)
+		{
+			cout << "Code Point for Character #" << count << ": U+00" << hex.front();
+			hex.pop();
+			cout << hex.front() << endl;
+			hex.pop();
+			fLetters.pop();
+			bNum.pop();
+			count++;
+		}
+		else if (bNum.front() == 2)
+		{
+			hex.pop();
+			hex.pop();
+			cout << "Code Point for Character #" << count << ": U+00" << hex.front();
+			hex.pop();
+			cout << hex.front() << endl;
+			fLetters.pop();
+			fLetters.pop();
+			hex.pop();
+			bNum.pop();
+			count++;
+		}
+		else if (bNum.front() == 3)
+		{
+			string first = fLetters.front().to_string();
+			fLetters.pop();
+			string second = fLetters.front().to_string();
+			fLetters.pop();
+			first = first.substr(4, 8);
+			second = "00" + second.substr(2, 8);
+			binToHex(first);
+			binToHex(second);
+		}
+		else
+			break;
+	}
 }
 
 int main()
@@ -196,6 +301,9 @@ int main()
 
 	printBytes(fLetters, bNum, size);
 
+	getCodePoint(fLetters, bNum, size);
+
 	system("pause");
 }
+
 
